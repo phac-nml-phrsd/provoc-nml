dc752bc3391e9302b548ba5f4b1c6d374400a034
DBecker7 <d7becker@gmail.com>
2022-04-08 13:39:01 -0400

    Uses current version of provoc (about to change)

--- scripts/run_one.R
+++ scripts/run_one.R
@@ -7,22 +7,53 @@ suppressPackageStartupMessages({
 })
 
 animal <- read.csv(here("data/clean", "nml.csv"))
-s1 <- animal[animal$sample == animal$sample[1],]
+#s1 <- animal[animal$sample == animal$sample[1],]
 
 varmat <- astronomize()
 
-s1$mutation <- s1$label
-s1$coverage <- s1$alt_dp + s1$ref_dp
-s1$count <- round(s1$frequency*s1$coverage, 0)
-coco <- s1
+animal$mutation <- animal$label
+animal$coverage <- animal$alt_dp + animal$ref_dp # TODO: Is this the correct coverage???
+animal$count <- round(animal$frequency*animal$coverage, 0)
+coco <- animal
 
 fused <- fuse(coco, varmat)
 
-res1 <- provoc(fused, method = "optimize")
+res1 <- provoc(fused, method = "optim")
+any(sapply(res1, function(x) x$convergence)) # FALSE is good
+
+res1a <- lapply(seq_along(res1), function(x) {
+    est <- res1[[x]]$point_est
+    est$convergence <- res1[[x]]$convergence
+    est$sample <- names(res1)[x]
+    est
+}) %>% bind_rows() 
+
+sample_info <- animal %>%
+    group_by(sample, date, region) %>%
+    tally()
+
+res1a <- left_join(res1a, sample_info, by = "sample")
+
+ggplot(res1a, aes(x = date, y = rho)) + 
+    geom_point() +
+    facet_wrap(~ variant) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+
+
+
 res2 <- provoc(fused, method = "runjags")
+any(sapply(res2, function(x) x$convergence))
 
-res2b <- melt_mcmc(res2, pivot = TRUE) %>%
-    group_by(name) %>%
-    summarise(par = median(value), .groups = "drop")
+res2a <- lapply(seq_along(res2), function(x) {
+    est <- res2[[x]]$point_est
+    est$convergence <- res2[[x]]$convergence
+    est$sample <- names(res2)[x]
+    est
+}) %>% bind_rows() 
 
-plot(as.numeric(res2b$par), as.numeric(res1$par))
+res2a <- left_join(res2a, sample_info, by = "sample")
+
+ggplot(res2a, aes(x = date, y = rho)) + 
+    geom_point() +
+    facet_wrap(~ variant)
