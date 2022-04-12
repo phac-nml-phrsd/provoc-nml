fba8c89b8a88672645ed5156c6d480e2630033fc
DBecker7 <d7becker@gmail.com>
2022-04-12 10:44:40 -0400

    I now have results.

--- scripts/run_one.R
+++ scripts/run_one.R
@@ -1,23 +1,31 @@
 suppressPackageStartupMessages({
     library(dplyr)
     library(ggplot2)
+    theme_set(theme_bw())
     library(provoc)
     library(lubridate)
     library(here)
 })
 
-animal <- read.csv(here("data/clean", "nml.csv"))
-#s1 <- animal[animal$sample == animal$sample[1],]
+animal <- read.csv(here("data/clean", "nml.csv")) 
+coco <- animal %>%
+    mutate(location = ifelse(grepl("MMN", sample), 
+        yes = "MMN", 
+        no = ifelse(grepl("MMS", sample),
+            yes = "MMS",
+            no = "Other")),
+        mutation = label,
+        coverage = alt_dp + ref_dp, # TODO: Is this correct for coverage?
+        count = round(frequency * coverage, 0))
 
 varmat <- astronomize()
 
-animal$mutation <- animal$label
-animal$coverage <- animal$alt_dp + animal$ref_dp # TODO: Is this the correct coverage???
-animal$count <- round(animal$frequency*animal$coverage, 0)
-coco <- animal
-
 fused <- fuse(coco, varmat)
 
+
+
+
+#### OPTIM ------------------------------------------------
 res1 <- provoc(fused, method = "optim")
 any(sapply(res1, function(x) x$convergence)) # FALSE is good
 
@@ -34,14 +42,11 @@ sample_info <- animal %>%
 
 res1a <- left_join(res1a, sample_info, by = "sample")
 
-ggplot(res1a, aes(x = date, y = rho)) + 
-    geom_point() +
-    facet_wrap(~ variant) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 
 
 
 
+#### RUNJAGS ----------------------------------------------
 res2 <- provoc(fused, method = "runjags")
 any(sapply(res2, function(x) x$convergence))
 
@@ -54,6 +59,33 @@ res2a <- lapply(seq_along(res2), function(x) {
 
 res2a <- left_join(res2a, sample_info, by = "sample")
 
-ggplot(res2a, aes(x = date, y = rho)) + 
-    geom_point() +
-    facet_wrap(~ variant)
+
+
+
+
+#### ALL TOGETHER NOW -------------------------------------
+for(i in seq_along(res1)) {
+    dfa <- bind_cols(res1[[i]]$point_est, res1[[i]]$sample_info)
+    dfa$method <- "optim"
+    dfb <- bind_cols(res2[[i]]$point_est, res2[[i]]$sample_info)
+    dfb$method <- "runjags"
+
+    if(i == 1) {
+        df <- bind_rows(dfa, dfb)
+    } else {
+        df <- bind_rows(df, dfa, dfb)
+    }
+}
+
+ggplot(df) + 
+    aes(x = date, y = rho, shape = method, colour = location) +
+    geom_point() + 
+    geom_line(aes(group = paste(method, location)), se = FALSE) +
+    facet_wrap(~ variant) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+ggplot(df) +
+    aes(x = date, y = rho, fill = variant) +
+    geom_col(colour = 1) +
+    facet_wrap(~ method + location) +
+    coord_flip()
