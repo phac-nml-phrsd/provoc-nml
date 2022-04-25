7cf956fb180ac45e46653d7bb2e1f3a9f3e94a4e
DBecker7 <d7becker@gmail.com>
2022-04-25 14:15:20 -0400

    Major refector; run all varmat defs

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -18,43 +18,111 @@ coco <- animal %>%
         coverage = alt_dp + ref_dp, # TODO: Is this correct for coverage?
         count = round(frequency * coverage, 0))
 
-varmat <- astronomize()
+varmat_types <- c("constellations", "varmat_from_variants-all_voc", "varmat_from_variants-omicron_delta", "varmat_from_data")
+varmat_type <- varmat_types[1]
 
-fused <- fuse(coco, varmat)
+for(varmat_type in varmat_types[1:3]) { # varmat_from_data causes errors - probably too big or with too many minor mutations
 
+    handle <- paste0("results/", varmat_type, ".RDS")
+    if(file.exists(handle)) {
+        res <- readRDS(handle)
+        res1 <- res$optim
+        res2 <- res$runjags
+    } else {
+        if(varmat_type == "constellations") {
+            varmat <- astronomize()
+        } else if (varmat_type == "varmat_from_variants-all_voc") {
+            all_voc <-  c(#"B.1.1.529", "BA.1", "BA.1.1", "BA.2", 
+                "B.1.1.7", "P.1", "P.2", "P.3", 
+                "B.1.617.2", "AY.1", "AY.2", "AY.4", "AY.4.2", 
+                "C.37", "B.1.621", "B.1.351", "B.1.525", "B.1.526", "B.1.617.1")
+            all_voc <- all_voc[all_voc %in% unique(mutations_by_lineage$lineage)]
+            varmat <- varmat_from_variants(variants = all_voc, 
+                mutation_format = "aa")
+        } else if (varmat_type == "varmat_from_variants-omicron_delta") {
+            varmat <- varmat_from_variants(variants = c("B.1.1.529", "BA.1", "BA.1.1", "BA.2", 
+                "B.1.617.2", "AY.1", "AY.2", "AY.4", "AY.4.2"), 
+                mutation_format = "aa")
+        } else if (varmat_type == "varmat_from_data") {
+            varmat <- varmat_from_data(type = coco$type, pos = coco$pos, alt = coco$alt, max_n = 30, mutation_format = "aa")
+        } else {
+            stop("No valid varmat type specified.")
+        }
 
+        fused <- fuse(coco, varmat)
+        dim(coco); dim(fused)
 
+        res1 <- provoc(fused = fused, method = "optim")
+        res1$method <- "optim"
+        res2 <- provoc(fused = fused, method = "runjags", quiet = 0)
+        res2$method <- "runjags"
+        saveRDS(list(optim = res1, runjags = res2), 
+            file = handle)
+    }
+    df <- bind_rows(res1, res2)
 
-#### OPTIM ------------------------------------------------
-res1 <- provoc(fused, method = "optim")
-convergence(res1)
+    pdf(file = paste0("results/", varmat_type, ".pdf"), width = 11, height = 8.5)
 
+    # Optim Results
+    gg_optim <- res1 %>% group_by(variant) %>%
+        mutate(include = rep(!all(rho < 0.05), n())) %>%
+        ungroup() %>% 
+        filter(include) %>%
+        ggplot(aes(x = ymd(date), y = rho, colour = location)) + 
+            geom_point() + 
+            facet_wrap(~ variant) +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+            scale_x_date(breaks = sort(unique(ymd(res1$date)))) +
+            labs(title = "Results from Optim")
+    print(gg_optim)
 
+    # RunJags Results
+    gg_runjags <- res2 %>% group_by(variant) %>%
+        mutate(include = rep(!all(rho < 0.05), n())) %>%
+        ungroup() %>% 
+        filter(include) %>%
+        ggplot(aes(x = ymd(date), y = rho, ymin = ci_low, ymax = ci_high, colour = location)) + 
+            geom_point() + geom_errorbar() +
+            facet_wrap(~ variant) +
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+            scale_x_date(breaks = sort(unique(ymd(res1$date)))) +
+            labs(title = "Results from RunJAGS")
+    print(gg_runjags)
 
+    # Both methods at once
+    gg_both <- ggplot(df) + 
+        aes(x = ymd(date), y = rho, shape = method, colour = location) +
+        geom_point() + 
+        geom_line(aes(group = paste(method, location))) +
+        facet_wrap(~ variant) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+        scale_x_date(breaks = sort(unique(ymd(res1$date)))) +
+        labs(title = "Results Everywhere")
+    print(gg_both)
 
+    # Both methods - Stacked Bar Chart
+    gg_stacked <- ggplot(res1) +
+        aes(x = ymd(date), y = rho, fill = variant) +
+        geom_col(colour = 1) +
+        facet_wrap(~ method + location) +
+        coord_flip()  +
+        scale_x_date(breaks = sort(unique(ymd(res1$date)))) +
+        labs(title = "Results Everywhere")
+    print(gg_stacked)
 
-#### RUNJAGS ----------------------------------------------
-res2 <- provoc(fused, method = "runjags")
-convergence(res2)
+    # Concordance between methods
+    gg_conc <- full_join(res1, res2, by = c("date", "location", "sample", "variant")) %>%
+        ggplot() +
+            aes(x = rho.x, y = rho.y) +
+            geom_point() + 
+            geom_abline(intercept = 0, slope = 1) +
+            coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
+            labs(x = "Optim", y = "RunJags") +
+            facet_wrap(~ variant) +
+            labs(title = "Concordance between methods")
+    print(gg_conc)
+    dev.off()
 
-
-
-
-
-#### ALL TOGETHER NOW -------------------------------------
-res1$method <- "optim"
-res2$method <- "runjags"
-df <- bind_rows(res1, res2)
-
-ggplot(df) + 
-    aes(x = date, y = rho, shape = method, colour = location) +
-    geom_point() + 
-    geom_line(aes(group = paste(method, location)), se = FALSE) +
-    facet_wrap(~ variant) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-
-ggplot(df) +
-    aes(x = date, y = rho, fill = variant) +
-    geom_col(colour = 1) +
-    facet_wrap(~ method + location) +
-    coord_flip()
+    convergence(res1)
+    convergence(res2)
+}
