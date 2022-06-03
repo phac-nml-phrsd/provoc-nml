0332d35df8bf384a0702c3576145edb905fda42b
DBecker7 <d7becker@gmail.com>
2022-06-03 13:34:44 -0400

    Binomial GLMs and simulation timing

--- scripts/compare_methods.R
+++ scripts/compare_methods.R
@@ -5,12 +5,13 @@ library(dplyr)
 library(ggplot2)
 theme_set(theme_bw())
 
-alcov <- function(coco, varmat, method = c("AlCoV-LM", "AlCoV-Robust", "AlCoV-NNLS")) {
+alcov <- function(coco, varmat, method = c("AlCoV-LM", "AlCoV-Robust", "AlCoV-NNLS", "AlCoV-Binom")) {
     freqs <- coco$count/coco$coverage
     df <- as.data.frame(t(varmat))
     df$freq <- freqs
 
     bind_rows(lapply(method, function(methi) {
+        t0 <- Sys.time()
         if(methi == "AlCoV-LM") {
             res <- summary(lm(freq ~ 0 + ., data = df))$coef[, 1:2]
         } else if(methi == "AlCoV-Robust"){
@@ -20,9 +21,11 @@ alcov <- function(coco, varmat, method = c("AlCoV-LM", "AlCoV-Robust", "AlCoV-NN
             freqs[freqs == 0] <- 0.0001
             res <- cbind(nnls(t(varmat), freqs)$x, NA)
             rownames(res) <- rownames(varmat)
+        } else if(methi == "AlCoV-Binom"){
+            res <- summary(glm(freq ~ ., data = df))$coef[-1, 1:2]
         }
         res <- data.frame(variant = rownames(res), rho = res[,1], se = res[,2], 
-            method = methi)
+            method = methi, time = as.numeric(difftime(Sys.time(), t0, units = "secs"))/nrow(res))
         row.names(res) <- NULL
         res
     }))
@@ -74,48 +77,65 @@ optim_methods <- function(coco, varmat,
 
 
     bind_rows(lapply(method, function(meth) {
+        t0 <- Sys.time()
         res <- stats::constrOptim(rho_init,
             f = objective, grad = NULL,
             ui = ui, ci = ci,
             count = coco$count, coverage = coco$coverage, varmat = varmat,
             methi = meth,
             control = list(maxit = 10000))
-        data.frame(variant = rownames(varmat), rho = res$par, method = meth)}))
+        data.frame(variant = rownames(varmat), rho = res$par, method = meth,
+            time = as.numeric(difftime(Sys.time(), t0, units = "secs"))/length(res$par))
+    }))
 }
 
-avg_freq <- function(fused, method = c("Simple Avg", "binomial", "quasibinomial")) {
+avg_freq <- function(fused, method = c("Simple Avg", "Simple Med", "Binomial_GLM", "Binomial_GLM_Quasi")) {
     vars <- which(startsWith(colnames(fused), "var_"))
     avg <- c()
     se <- c()
+    times <- c()
     for(i in vars) {
+        t0 <- Sys.time()
         others <- vars[vars != i]
         sub_fuse <- fused[fused[,i] == 1 &
             apply(fused[, others, drop = FALSE], 1, sum) == 0,]
-        if(method[1] == "Simple Avg") {
+        if(method[1] %in% c("Simple Avg", "Simple Med")) {
             if(nrow(sub_fuse) < 1) {
-                avg <- c(avg, 0)
+                avg <- c(avg, NA)
                 se <- c(se, NA)
             } else {
-                counts <- sub_fuse$count
-                covers <- sub_fuse$coverage
-                avg_tmp <- counts/covers
-                avg_tmp[covers == 0] <- 0
-                avg <- c(avg, mean(avg_tmp))
-                se <- c(se, NA)
+                avg_tmp <- sub_fuse$count/sub_fuse$coverage
+                avg_tmp[sub_fuse$coverage == 0] <- 0
+                if(method[1] == "Simple Avg") {
+                    avg <- c(avg, mean(avg_tmp))
+                    se <- c(se, sd(avg_tmp))
+                } else {
+                    avg <- c(avg, median(avg_tmp))
+                    se <- c(se, IQR(avg_tmp))
+                }
             }
         } else {
-            res <- glm(cbind(count, coverage - count) ~ 1, 
-                data = sub_fuse, 
-                family = ifelse(method[1] == "binomial", "binomial", "quasibinomial"))
-            avg <- c(avg, res$coef[1])
-            se <- c(se, summary(res)$coef[1,2])
+            successes <- sub_fuse$count
+            failures <- sub_fuse$coverage - successes
+            if(length(successes) > 2) {
+                res <- glm(cbind(successes, failures) ~ 1,
+                    family = ifelse(method[1] == "Binomial_GLM", binomial, quasibinomial))
+                avg <- c(avg, exp(res$coef)/(1 + exp(res$coef)))
+                se_tmp <- summary(res)$coef[1,2]
+                se <- c(se, exp(se_tmp)/(1 + exp(se_tmp)))
+            } else {
+                avg <- c(avg, NA)
+                se <- c(se, NA)
+            }
         }
+        times <- c(times, as.numeric(difftime(Sys.time(), t0, units = "secs")))
     }
-    if(sum(avg) > 1) avg <- avg / sum(avg)
+    #if(sum(avg) > 1) avg <- avg / sum(avg)
     data.frame(variant = gsub("var_", "", fixed = TRUE, names(fused[, vars])),
         rho = avg,
         se = se,
-        method = method)
+        method = method,
+        time = times)
 }
 
 
@@ -128,13 +148,13 @@ truth <- list(
         variants = c("B.1.617.2", "B.1.617.2+K417N"),
         probs = c(0.97, 0.3)
     ),
-    Too_Close_to_Call = data.frame(
+    Too_Close = data.frame(
         variants = c("B.1.617.2", "B.1.617.2+K417N", "A.23.1", "A.23.1+E484K", 
-            "BA.1", "BA.2"),
+            "AY.4.2", "AY.4"),
         probs = c(0.5, 0.03, 0.2, 0.02, 0.2, 0.05)
     ),
     Total_Below_One = data.frame(
-        variants = c("BA.1", "BA.2", "B.1.617.2"),
+        variants = c("BA.1", "BA.2", "B.1.1.529"),
         probs = c(0.2, 0.6, 0.2)
     ),
     One_Two_Punch = data.frame(
@@ -148,6 +168,7 @@ truth <- list(
 )
 
 for (scenario in names(truth)) {
+    print(scenario)
     varmat <- astronomize()
     varmat <- varmat[rownames(varmat) %in% truth[[scenario]]$variants,, drop = FALSE]
     varmat <- varmat[, apply(varmat, 2, sum) > 0]
@@ -176,7 +197,10 @@ for (scenario in names(truth)) {
         all_res_tmp <- bind_rows(
             alcov(coco, varmat2),
             optim_methods(coco, varmat2),
-            avg_freq(fused, method = "Simple Avg")
+            avg_freq(fused, method = "Simple Avg"),
+            avg_freq(fused, method = "Simple Med"),
+            avg_freq(fused, method = "Binomial_GLM"),
+            avg_freq(fused, method = "Binomial_GLM_Quasi")
         )
         all_res_tmp$iter <- i
         if(i == 1) {
@@ -215,3 +239,27 @@ ggplot(all_res3, aes(x = method, y = rho - prob, fill = method)) +
     facet_grid(scenario ~ variant, scales = "free") +
     geom_hline(yintercept = 0) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+all_res3 %>%
+    filter(!is.na(rho), !is.na(method), !is.na(scenario), !is.na(prob)) %>%
+    group_by(method, scenario, variant) %>%
+    summarise(abs_bias = abs(rho - prob), mse = mean((rho - prob)^2), var = var(rho), .groups = "drop") %>%
+    group_by(method, scenario) %>%
+    summarise(abs_bias = mean(abs_bias), mse = mean(mse), var = mean(var)) %>%
+    #tidyr::pivot_longer(cols = c(abs_bias, var, mse)) %>%
+    ggplot() +
+        aes(x = method, y = mse, fill = method) +
+        geom_col(position = "dodge", colour = "white") +
+        facet_wrap( ~ scenario , scales = "free") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+all_res3 %>%
+    filter(variant != "total") %>%
+    group_by(scenario, iter, method) %>%
+    summarise(time = sum(time, na.rm = TRUE), .groups = "drop") %>%
+    ggplot() +
+        aes(x = method, y = time, fill = method) %>%
+        geom_violin(scale = "width") +
+        facet_wrap(~ scenario) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+        labs(y = "Time (Seconds)")
