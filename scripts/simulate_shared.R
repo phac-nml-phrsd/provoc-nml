2e8afbf3de5aa72258db76315940986c6bc467c6
DBecker7 <d7becker@gmail.com>
2022-06-14 14:13:48 -0400

    Move random mutations to replicates loop

--- scripts/simulate_shared.R
+++ scripts/simulate_shared.R
@@ -12,12 +12,12 @@ for (i in 0:19) {
     print(i)
     X1 <- c(rep(1, 20), rep(0, 20 - i), rep(0, 5))
     X2 <- c(rep(0, 20 - i), rep(1, 20), rep(0, 5))
-    X3 <- rbinom(length(X1), prob = 0.5, size = 1)
-    varmat <- do.call(rbind, list(X1, X2, X3))
-    rownames(varmat) <- paste0("Variant", 1:nrow(varmat))
-    colnames(varmat) <- paste0("M", 1:ncol(varmat))
 
-    for(ii in 1:100) {
+    for(ii in 1:1000) {
+        X3 <- rbinom(length(X1), prob = 0.5, size = 1)
+        varmat <- do.call(rbind, list(X1, X2, X3))
+        rownames(varmat) <- paste0("Variant", 1:nrow(varmat))
+        colnames(varmat) <- paste0("M", 1:ncol(varmat))
         coco <- simulate_coco(varmat, rel_counts = c(420, 120, 60), verbose = FALSE)
         fused <- fuse(coco, varmat, verbose = FALSE)
         res <- bind_rows(
@@ -55,17 +55,18 @@ all_res_stats <- all_res %>%
         s = sd(rho, na.rm = TRUE),
         t = mean(time))
 
+ggplot(all_res_stats) +
+    aes(x = overlap, y = t, colour = method) +
+    geom_point() + geom_line() +
+    facet_wrap(~ variant)
+ggplot(filter(all_res_stats, method %in% c("freyja", "AlCoV-LM", "Simple Avg", "Simple Med", "provoc"))) +
+    aes(x = overlap, y = m, colour = method) +
+    geom_point() + geom_line() +
+    facet_wrap(~ variant)
+
 ggplot(filter(all_res_stats, method %in% c("freyja", "AlCoV-LM", "Simple Avg", "Simple Med", "provoc"))) +
     aes(x = overlap, y = s, colour = method) +
     geom_point() + geom_line() +
     facet_wrap(~ variant) +
     labs(x = "Number Overlapping Mutations",
         y = "Standard Deviation of Estimates")
-ggplot(filter(all_res_stats, method %in% c("freyja", "AlCoV-LM", "Simple Avg", "Simple Med", "provoc"))) +
-    aes(x = overlap, y = m, colour = method) +
-    geom_point() + geom_line() +
-    facet_wrap(~ variant)
-ggplot(all_res_stats) +
-    aes(x = overlap, y = t, colour = method) +
-    geom_point() + geom_line() +
-    facet_wrap(~ variant)
