40624397dd30a0507d4cf7b696a591c1bde76e1c
DBecker7 <d7becker@gmail.com>
2022-07-12 10:18:13 -0400

    More mutation label fixing

--- scripts/ww_benchmark.R
+++ scripts/ww_benchmark.R
@@ -69,7 +69,7 @@ print(paste0("Data intake took ", t_intake))
 t1 <- Sys.time()
 
 m_rep <- readRDS(here("output", "mutations.RDS"))
-varmat <- varmat_from_variants(consensuses, max_n = 400, top_quantile = 0, mutations = m_rep)
+varmat <- varmat_from_variants(consensuses, max_n = 400, top_quantile = 0, mutations = m_rep, mutation_format = "aa")
 varmat <- varmat[, colSums(varmat) > 1]
 varmat <- varmat[rowSums(varmat) > 0, ]
 
@@ -90,6 +90,11 @@ for (i in seq_along(varmut)) {
     }
 }
 colnames(varmat) <- varmut
+
+
+mut_bad <- which(is.na(match(colnames(varmat), coco$mutation)))
+sort(colnames(varmat)[mut_bad])
+
 
 coco <- full_join(
     mfiles[mfiles$position %in% varpos,], 
@@ -184,12 +189,40 @@ print(paste0("Total Rscript: ", difftime(Sys.time(), t0, units = "secs")))
 
 varmat <- astronomize()
 varmat <- varmat[rownames(varmat) %in% c("B.1.617.2", 
-    "BA.1", "BA.2"),]
+    "BA.1", "BA.2", "AY.4", "AY.4.2"),]
+varmat <- varmat[, colSums(varmat) > 0]
+colnames(varmat)[which(colnames(varmat) == "+2205.GAGCCAGAA")] <- "ins:22205:9"
+colnames(varmat)[which(colnames(varmat) == "+8262.AACA")] <- "ins:28262:4"
+colnames(varmat)[which(colnames(varmat) == "28271-")] <- "del:28271:1"
+# Spike protein starts at position 21562
+# Position reported as index of amino acids, hence 3*246
+colnames(varmat)[which(colnames(varmat) == "aa:S:RSYLTPG246-")] <- paste0("del:", 21562 + 3*246, ":21")
+colnames(varmat)[which(colnames(varmat) == "aa:S:Y144-")] <- paste0("del:", 21562 + 3*144, ":1")
+colnames(varmat)[which(colnames(varmat) == "aa:S:HV69-")] <- paste0("del:", 21562 + 3*69, ":2")
+# ORF 1a starts at 265
+colnames(varmat)[which(colnames(varmat) == "aa:orf1a:SGF3675-")] <- paste0("del:", 265 + 3*3675 - 1, ":9")
+
 coco <- full_join(mfiles, cfiles, by = c("position", "sample"))
 coco$coverage <- apply(coco[, c("coverage.x", "coverage.y")], 1, 
     mean, na.rm = TRUE)
 coco$coverage.x <- coco$coverage.y <- NULL
 coco$count <- coco$frequency * coco$coverage
+
+
+mut_bad <- which(is.na(match(colnames(varmat), coco$mutation)))
+mut_bad <- mut_bad[!grepl(":", colnames(varmat)[mut_bad])]
+for(i in mut_bad){
+    col_bad <- colnames(varmat)[i]
+    col_good <- parse_mutation("~", 
+        substr(col_bad, 1, nchar(col_bad) - 1),
+        substr(col_bad, nchar(col_bad), nchar(col_bad)))
+    colnames(varmat)[i] <- col_good
+
+}
+
+mut_bad <- which(is.na(match(colnames(varmat), coco$mutation)))
+sort(colnames(varmat)[mut_bad])
+
 fused <- fuse(coco, varmat)
 
 f2 <- fused %>%
@@ -243,6 +276,7 @@ for(i in mut_bad){
 
 fused <- fuse(coco, varmat)
 
+# Remove samples with too few Omicron mutations
 f2 <- fused %>%
     group_by(sample) %>%
     mutate(keep = n() > 15) %>%
@@ -278,4 +312,78 @@ full_join(res3, res4, by = c("variant", "sample")) %>%
         facet_wrap(~sample) +
         coord_flip()
 
+bind_rows(res3, res4) %>%
+    ggplot() +
+        aes(x = variant, y = rho, colour = variant, shape = mutation_list) +
+        geom_point() +
+        facet_wrap(~ sample) +
+        coord_flip()
+
 write.csv(res4, file = here("output", "ww_results_gromstollations.csv"))
+
+
+varmat <- astronomize()
+varmat <- varmat[rownames(varmat) %in% c("B.1.617.2", 
+    "BA.1", "BA.2", "AY.4", "AY.4.2"),]
+varmat <- varmat[, colSums(varmat) > 0]
+colnames(varmat)[which(colnames(varmat) == "+2205.GAGCCAGAA")] <- "ins:22205:9"
+colnames(varmat)[which(colnames(varmat) == "+8262.AACA")] <- "ins:28262:4"
+colnames(varmat)[which(colnames(varmat) == "28271-")] <- "del:28271:1"
+# Spike protein starts at position 21562
+# Position reported as index of amino acids, hence 3*246
+colnames(varmat)[which(colnames(varmat) == "aa:S:RSYLTPG246-")] <- paste0("del:", 21562 + 3*246, ":21")
+colnames(varmat)[which(colnames(varmat) == "aa:S:Y144-")] <- paste0("del:", 21562 + 3*144, ":1")
+colnames(varmat)[which(colnames(varmat) == "aa:S:HV69-")] <- paste0("del:", 21562 + 3*69, ":2")
+# ORF 1a starts at 265
+colnames(varmat)[which(colnames(varmat) == "aa:orf1a:SGF3675-")] <- paste0("del:", 265 + 3*3675 - 1, ":9")
+
+garmat <- astronomize(path = here("data", "gromstollations"))
+# Manual fix
+colnames(garmat)[which(colnames(garmat) == "+2205.GAGCCAGAA")] <- "ins:22205:9"
+
+varmat <- varmat[!rownames(varmat) %in% rownames(garmat),]
+varmat <- bind_rows(as.data.frame(varmat), as.data.frame(garmat))
+varmat <- as.matrix(varmat)
+varmat[is.na(varmat)] <- 0
+varmat <- varmat[, colSums(varmat) > 0]
+varmat[, 1:5]
+
+mut_bad <- which(is.na(match(colnames(varmat), coco$mutation)))
+mut_bad <- mut_bad[!grepl(":", colnames(varmat)[mut_bad])]
+for(i in mut_bad){
+    col_bad <- colnames(varmat)[i]
+    col_good <- parse_mutation("~", 
+        substr(col_bad, 1, nchar(col_bad) - 1),
+        substr(col_bad, nchar(col_bad), nchar(col_bad)))
+    colnames(varmat)[i] <- col_good
+
+}
+
+coco <- full_join(mfiles, cfiles, by = c("position", "sample"))
+coco$coverage <- apply(coco[, c("coverage.x", "coverage.y")], 1, 
+    mean, na.rm = TRUE)
+coco$coverage.x <- coco$coverage.y <- NULL
+coco$count <- coco$frequency * coco$coverage
+
+fused <- fuse(coco, varmat)
+
+# Remove samples with too few Omicron mutations
+f2 <- fused %>%
+    group_by(sample) %>%
+    mutate(keep = n() > 15) %>%
+    ungroup() %>%
+    filter(keep) %>%
+    select(-keep)
+
+res5 <- provoc(fused = f2, method = "optim")
+res5$mutation_list <- "Delta and Omicron Gromstollations 2"
+
+res5[, c("ci_low", "ci_high")] <- NULL
+bind_rows(res3, res4, res5) %>%
+    ggplot() +
+        aes(x = variant, y = rho, colour = variant, shape = mutation_list) +
+        geom_point() +
+        facet_wrap(~ sample) +
+        coord_flip()
+
+write.csv(bind_rows(res3, res4, res5), file = here("output", "ww_results_gromstollations2.csv"))
